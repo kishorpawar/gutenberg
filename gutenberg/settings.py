dfdd65e9f6f7d84ce67562cfa75e85e69fb61c5a
Kishor Pawar <dm.kishorpawar@gmail.com>
2025-05-21 20:07:54 +0530

    typo fixed in settings

--- gutenberg/settings.py
+++ gutenberg/settings.py
@@ -100,7 +100,7 @@ WSGI_APPLICATION = 'gutenberg.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),,
+        'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST', default='localhost'),
